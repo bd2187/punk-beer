8c8c32f4fcded230fe506bf48d9f675528e2d90c
bd2187 <brandon.dionisio.26@gmail.com>
2020-01-20 21:07:25 -0800

    Add ternary operator which checks if beer.image_url exists. If it doesn't exist, load default image

--- src/BeerThumbnail.js
+++ src/BeerThumbnail.js
@@ -3,7 +3,14 @@ import React from "react";
 const BeerThumbnail = function({ beer }) {
     return (
         <li>
-            <img src={beer.image_url} alt={`${beer.name} beer`} />
+            <img
+                src={
+                    beer.image_url
+                        ? beer.image_url
+                        : "https://images.punkapi.com/v2/keg.png"
+                }
+                alt={`${beer.name} beer`}
+            />
             <h1>{beer.name}</h1>
             <p>{beer.tagline}</p>
         </li>
