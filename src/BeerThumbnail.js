fad38f4bc7fdcac244fdb7c1371b4d81c581395f
bd2187 <brandon.dionisio.26@gmail.com>
2020-01-21 21:48:12 -0800

    Wrap contents of <li> tag in Link component

--- src/BeerThumbnail.js
+++ src/BeerThumbnail.js
@@ -1,18 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const BeerThumbnail = function({ beer }) {
     return (
         <li>
-            <img
-                src={
-                    beer.image_url
-                        ? beer.image_url
-                        : "https://images.punkapi.com/v2/keg.png"
-                }
-                alt={`${beer.name} beer`}
-            />
-            <h1>{beer.name}</h1>
-            <p>{beer.tagline}</p>
+            <Link to={`/beer/${beer.id}`}>
+                <img
+                    src={
+                        beer.image_url
+                            ? beer.image_url
+                            : "https://images.punkapi.com/v2/keg.png"
+                    }
+                    alt={`${beer.name} beer`}
+                />
+                <h1>{beer.name}</h1>
+                <p>{beer.tagline}</p>
+            </Link>
         </li>
     );
 };
