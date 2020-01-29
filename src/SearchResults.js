aee9c7698e63f1de1a34bf53a635f75ce3ae464e
bd2187 <brandon.dionisio.26@gmail.com>
2020-01-28 20:28:57 -0800

    Add logic which renders searched beer

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import BeerThumbnail from "./BeerThumbnail";
 
 const SearchResults = function({ match }) {
-    return <h1>search results</h1>;
+    const [state, setState] = useState({
+        loading: true,
+        error: false,
+        searchResults: []
+    });
+
+    let { query } = match.params;
+    query = decodeURIComponent(query);
+
+    useEffect(
+        function() {
+            fetch(
+                `https://api.punkapi.com/v2/beers/?beer_name=${query.replace(
+                    / /g,
+                    "_"
+                )}`
+            )
+                .then(res => res.json())
+                .then(parsedRes => {
+                    setState({
+                        loading: false,
+                        error: false,
+                        searchResults: parsedRes
+                    });
+                })
+                .catch(() => {
+                    setState({
+                        loading: false,
+                        error: true
+                    });
+                });
+        },
+        [match.params]
+    );
+
+    if (state.error) {
+        return <h1>error!</h1>;
+    } else if (state.loading) {
+        return <h1>loading</h1>;
+    } else if (state.searchResults.length > 0) {
+        return (
+            <>
+                <h1>{state.searchResults.length} results found</h1>
+
+                <ul>
+                    {state.searchResults.map(beer => (
+                        <BeerThumbnail key={beer.id} beer={beer} />
+                    ))}
+                </ul>
+            </>
+        );
+    } else {
+        return <h1>no beer found under &quot;{query}&quot;</h1>;
+    }
 };
 
 export default SearchResults;
