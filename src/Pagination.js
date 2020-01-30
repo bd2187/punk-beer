4f20fdaa7ce91e95bfefd33904f815246ccc208d
bd2187 <brandon.dionisio.26@gmail.com>
2020-01-29 21:03:44 -0800

    Avoid iterating through each page number in render(). Add lifecycle methods to handle adding and updating pages

--- src/Pagination.js
+++ src/Pagination.js
@@ -4,40 +4,73 @@ import { Link } from "react-router-dom";
 class Pagination extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            currentPage: 0,
+
+            pages: []
+        };
     }
 
     static getDerivedStateFromProps(props) {
         return { currentPage: props.currentPage };
     }
 
-    render() {
-        const pages = [];
-        const { maxPages, link } = this.props;
-        const currentPage = +this.state.currentPage;
+    componentDidMount() {
+        let pages = [];
 
-        for (let i = 1; i <= maxPages; i++) {
-            var style = i === +currentPage ? { color: "red" } : {};
-
-            pages.push(
-                <li key={i} style={style}>
-                    <Link to={`${link}${i}`}>{i}</Link>
-                </li>
-            );
+        for (let i = 1; i <= this.props.maxPages; i++) {
+            pages = [
+                ...pages,
+                { number: i, selected: +this.state.currentPage === i }
+            ];
         }
 
+        this.setState({ pages });
+    }
+
+    componentDidUpdate({ currentPage: previousPage }) {
+        if (+previousPage !== +this.state.currentPage) {
+            // Update selected page
+            const pages = [...this.state.pages].map(page => {
+                page.selected = +page.number === +this.state.currentPage;
+                return page;
+            });
+
+            this.setState({ pages });
+        }
+    }
+
+    render() {
         return (
             <div>
-                {currentPage - 1 > 0 ? (
-                    <Link to={`${link}${currentPage - 1}`}>Prev</Link>
-                ) : null}
-                <ul>{pages}</ul>
-                {currentPage + 1 <= maxPages ? (
-                    <Link to={`${link}${currentPage + 1}`}>Next</Link>
-                ) : null}
+                <ul>
+                    {this.state.pages.map(({ number, selected }) => {
+                        var color = selected ? "red" : "black";
+                        return (
+                            <li key={number}>
+                                <Link
+                                    to={`${this.props.link}${number}`}
+                                    style={{ color }}
+                                >
+                                    {number}
+                                </Link>
+                            </li>
+                        );
+                    })}
+                </ul>
             </div>
         );
     }
 }
 
 export default Pagination;
+
+//             <div>
+//                 {currentPage - 1 > 0 ? (
+//                     <Link to={`${link}${currentPage - 1}`}>Prev</Link>
+//                 ) : null}
+//                 <ul>{pages}</ul>
+//                 {currentPage + 1 <= maxPages ? (
+//                     <Link to={`${link}${currentPage + 1}`}>Next</Link>
+//                 ) : null}
+//             </div>
